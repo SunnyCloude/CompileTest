6e86ca746517e0180f34be8b725a51f733f17352
SunnyCloude <729930935@qq.com>
2024-11-21 21:28:20 +0800

    First Commit!

---
+++ Source/TencentCourceWork/TencentCourceWork.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TencentCourceWork.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TencentCourceWork, "TencentCourceWork" );
+ 
---
+++ Source/TencentCourceWork/TencentCourceWorkGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TencentCourceWorkGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATencentCourceWorkGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATencentCourceWorkGameMode();
+};
+
+
+
